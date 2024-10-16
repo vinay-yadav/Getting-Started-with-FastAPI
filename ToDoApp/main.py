cd90b31132ab69e95dffe3c4822322948c1e0001
Vinay Yadav <yadav.vinay1508@gmail.com>
2024-10-16 23:45:20 +0530

    Initialised with ToDoApp

---
+++ ToDoApp/main.py
@@ -0,0 +1,13 @@
+import models
+
+from fastapi import FastAPI
+from database import engine
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+async def create_database():
+    return "Database created"
