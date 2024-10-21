ebf451af671d555982e2307dabade8f54e272f97
Vinay Yadav <yadav.vinay1508@gmail.com>
2024-10-21 23:34:22 +0530

    Added User table

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,5 +1,20 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship('ToDo', back_populates='owner')
 
 
 class ToDo(Base):
@@ -10,6 +25,9 @@ class ToDo(Base):
     description = Column(String)
     priority = Column(Integer)
     completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship("User", back_populates="todos")
 
 # insert into todos (title, description, priority, completed) values ("Go to the store", "Pick-up eggs", 5, False)
 # insert into todos (title, description, priority, completed) values ("Cut the lawn", "Grass is getting long", 3, False)
