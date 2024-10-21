75277a8d7f561eb70a64f4d9b43e56c28e1e39fa
Vinay Yadav <yadav.vinay1508@gmail.com>
2024-10-22 01:29:15 +0530

    added auth APIs

---
+++ ToDoApp/auth.py
@@ -0,0 +1,124 @@
+import models
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel
+from typing import Optional
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+
+models.Base.metadata.create_all(bind=engine)
+
+SECRET_KEY = "THis is a secret key"
+ALGO = "HS256"
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+app = FastAPI()
+
+
+def get_password_hash(password: str) -> str:
+    return bcrypt_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+@app.post("/login")
+async def login(username: str, password: str, db):
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGO)
+
+
+async def get_current_user(token: str = Depends(oauth2_bearer)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise get_user_exception()
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise get_user_exception()
+
+
+@app.post("/create_user")
+async def create_user(request: CreateUser, db: Session = Depends(get_db)):
+    user_model = models.User(
+        email=request.email,
+        username=request.username,
+        last_name=request.last_name,
+        first_name=request.first_name,
+        password=get_password_hash(request.password),
+        is_active=True
+    )
+
+    db.add(user_model)
+    db.commit()
+    return user_model
+
+
+@app.post("/token")
+async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = await login(form_data.username, form_data.password, db)
+    if not user:
+        raise token_exception()
+    token_expire = timedelta(minutes=20)
+    token = create_access_token(username=user.username, user_id=user.id, expires_delta=token_expire)
+    return {"token": token}
+
+
+# Exceptions
+def get_user_exception():
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return credentials_exception
+
+
+def token_exception():
+    token_exception_response = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return token_exception_response
