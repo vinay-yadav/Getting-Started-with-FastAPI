c57f06c295c24b58549a53babd894e9425cd11e4
Vinay <yadav.vinay1508@gmail.com>
2025-05-04 02:47:45 +0530

    Updated models with new columns

--- src/api/events/models.py
+++ src/api/events/models.py
@@ -14,14 +14,19 @@ from sqlmodel import Field, SQLModel
 
 class EventModel(TimescaleModel, table=True):
     page: str = Field(index=True)
-    description: Optional[str] = ""
-    updated_at: datetime = Field(
-        default_factory=get_utc_now, sa_type=sqlmodel.DateTime(timezone=True), nullable=False
-    )
+    user_agent: Optional[str] = Field(default="", index=True)
+    ip_address: Optional[str] = Field(default="", index=True)
+    referrer: Optional[str] = Field(default="", index=True)
+    session_id: Optional[str] = Field(index=True)
+    duration: Optional[int] = Field(default=0)
 
     __chunk_time_interval__ = "INTERVAL 1 day"
     __drop_after__ = "INTERVAL 3 months"
-    
+
+    # description: Optional[str] = ""
+    # updated_at: datetime = Field(
+    #     default_factory=get_utc_now, sa_type=sqlmodel.DateTime(timezone=True), nullable=False
+    # )
     # id: Optional[int] = Field(default=None, primary_key=True)
     # created_at: datetime = Field(
     #     default_factory=get_utc_now, sa_type=sqlmodel.DateTime(timezone=True), nullable=False
@@ -30,16 +35,31 @@ class EventModel(TimescaleModel, table=True):
 
 class EventCreateSchema(SQLModel):
     page: str
-    description: Optional[str] = Field(default="My description")
+    user_agent: Optional[str] = Field(default="", index=True)
+    ip_address: Optional[str] = Field(default="", index=True)
+    referrer: Optional[str] = Field(default="", index=True)
+    session_id: Optional[str] = Field(index=True)
+    duration: Optional[int] = Field(default=0)
+
+    # description: Optional[str] = Field(default="My description")
 
 
-class EventUpdateSchema(SQLModel):
-    page: Optional[str] = ""
-    description: str
+# class EventUpdateSchema(SQLModel):
+#     page: Optional[str] = ""
+#     description: str
 
 
 # {"id": 123}
 
 class EventListSchema(SQLModel):
     results: List[EventModel]
+    count: int
+
+
+class EventBucketSchema(SQLModel):
+    bucket: datetime
+    page: str
+    ua: Optional[str] = ""
+    operating_system: Optional[str] = ""
+    avg_duration: Optional[float] = 0.00
     count: int
--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,23 +1,69 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session, select
+from typing import List
 
-from .models import EventModel, EventListSchema, EventCreateSchema, EventUpdateSchema, get_utc_now
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlmodel import Session, select
+from sqlalchemy import func, case
+from timescaledb.hyperfunctions import time_bucket
+
+from .models import EventModel, EventBucketSchema, EventCreateSchema, get_utc_now
 
 from api.db.session import get_session
 
 router = APIRouter()
 
+DEFAULT_LOOKUP_PAGES = [
+    "/", "/about", "/contact", "/pages", "/pricing", "/blog", "/products", "/login", "/signup", "/dashboard",
+    "/settings"
+]
+
 
 # /api/events/
-@router.get("/", response_model=EventListSchema)
-def read_events(session: Session = Depends(get_session)):
+@router.get("/", response_model=List[EventBucketSchema])
+def read_events(
+        duration: str = Query(default="1 day"),
+        pages: List = Query(default=None),
+        session: Session = Depends(get_session)
+):
     # a bunch of items in a table
-    query = select(EventModel).order_by(EventModel.id.desc()).limit(10)
-    results = session.exec(query).all()
-    return {
-        "results": results,
-        "count": len(results)
-    }
+    # query = select(EventModel).order_by(EventModel.id.desc()).limit(10)
+
+    os_case = case(
+        (EventModel.user_agent.ilike("%windows%"), "Windows"),
+        (EventModel.user_agent.ilike("%macintosh%"), "MacOS"),
+        (EventModel.user_agent.ilike("%iphone%"), "iOS"),
+        (EventModel.user_agent.ilike("%android%"), "Android"),
+        (EventModel.user_agent.ilike("%linux%"), "Linux"),
+        else_="Other"
+    ).label("operating_system")
+
+    bucket = time_bucket("1 day", EventModel.time)
+    lookup_pages = pages if isinstance(pages, list) and len(pages) > 0 else DEFAULT_LOOKUP_PAGES
+
+    query = (
+        select(
+            bucket.label("bucket"),
+            os_case,
+            EventModel.page.label("page"),
+            func.avg(EventModel.duration).label("avg_duration"),
+            func.count().label("count"),
+        )
+        .where(
+            EventModel.page.in_(lookup_pages)
+        )
+        .group_by(
+            bucket,
+            os_case,
+            EventModel.page,
+        )
+        .order_by(
+            bucket,
+            os_case,
+            EventModel.page,
+        )
+    )
+
+    results = session.exec(query).fetchall()
+    return results
 
 
 @router.post("/", response_model=EventModel)
@@ -39,23 +85,23 @@ def get_event(event_id: int, session: Session = Depends(get_session)):
     return result
 
 
-@router.put("/{event_id}", response_model=EventModel)
-def update_event(event_id: int, payload: EventUpdateSchema, session: Session = Depends(get_session)):
-    query = select(EventModel).where(EventModel.id == event_id)
-    obj = session.exec(query).first()
-    if not obj:
-        raise HTTPException(status_code=404, detail="Event not found")
-
-    data = payload.model_dump()
-
-    for k, v in data.items():
-        setattr(obj, k, v)
-    obj.updated_at = get_utc_now()
-
-    session.add(obj)
-    session.commit()
-    session.refresh(obj)
-    return obj
+# @router.put("/{event_id}", response_model=EventModel)
+# def update_event(event_id: int, payload: EventUpdateSchema, session: Session = Depends(get_session)):
+#     query = select(EventModel).where(EventModel.id == event_id)
+#     obj = session.exec(query).first()
+#     if not obj:
+#         raise HTTPException(status_code=404, detail="Event not found")
+#
+#     data = payload.model_dump()
+#
+#     for k, v in data.items():
+#         setattr(obj, k, v)
+#     obj.updated_at = get_utc_now()
+#
+#     session.add(obj)
+#     session.commit()
+#     session.refresh(obj)
+#     return obj
 
 
 @router.delete("/{event_id}")
